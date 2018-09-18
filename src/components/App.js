d2ccec8b4c532b4bbabf5683e50a4aef40665b1b
Bycor Sánchez <bycor1989@gmail.com>
2018-09-18 19:54:34 +0100

    feat: display each image on a slice

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,46 @@
 import '../css/App.css';
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import Slice from './Slice';
+import images from '../data/images.json';
 
 
 class App extends Component {
 
-  static propTypes = {
-    slices: PropTypes.number.isRequired
-  }
-
   state = {
+    images: [],
     hoverItem: undefined,
     selected: undefined
   }
 
+  componentDidMount() {
+    this.setState({ images })
+  }
+
   onHover = item => this.setState({ hoverItem: item });
   onClick = item => this.setState({ selected: item });
-
   isHover = item => this.state.hoverItem === item;
+  isSelected = item => this.state.selected === item;
 
   render() {
-    const { slices } = this.props;
-    const width = 100 / (slices - 1);
+    const { images } = this.state;
+    const width = 100 / (images.length - 1);
 
     return (
       <div className="App">
         {
-          Array(slices).fill()
-            .map((n, i) => (
-              <Slice
-                key={i}
-                item={i}
-                width={width}
-                isHover={this.isHover(i)}
-                onHover={this.onHover}
-                onClick={this.onClick}
-              />
-            ))
+          images &&
+          images.map((image, index) => (
+            <Slice
+              key={index}
+              item={index}
+              image={image}
+              width={width}
+              isHover={this.isHover(index)}
+              isSelected={this.isSelected(index)}
+              onHover={this.onHover}
+              onClick={this.onClick}
+            />
+          ))
         }
       </div>
     );
--- src/components/Slice.js
+++ src/components/Slice.js
@@ -3,30 +3,41 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
 const Slice = props => {
-    const { item, width, isHover, onHover, onClick } = props;
-    const offset = isHover ? (width / 4) : 0;
+    const { image, item, isHover, isSelected, onHover, onClick } = props;
 
     return (
         <div
-            className={classnames("slice", { "slice-hover": isHover })}
-            style={polygonShape(item, width, offset)}
+            className={classnames("slice", { "slice-front": (isHover || isSelected) })}
+            style={{
+                clipPath: clipPath(props),
+                backgroundImage: "url(" + image.url + ")"
+            }}
             onMouseEnter={() => onHover(item)}
             onClick={() => onClick(item)}
         />
     );
 }
 
-const polygonShape = (i, width, offset = 0) => {
+const clipPath = (props) => {
+    const { item, width, isSelected, isHover } = props;
+    return isSelected ? fullscreen() : polygon(item, width, isHover ? (width / 4) : 0);
+}
+
+const polygon = (i, width, offset = 0) => {
     const x1 = i * width;
     const x2 = (i + 1) * width;
     const x3 = (i - 1) * width;
-    return ({ "clipPath": "polygon(" + (x1 - offset) + "% 0," + (x2 + offset) + "% 0," + (x1 + offset) + "% 100%, " + (x3 - offset) + "% 100%)" });
+    return `polygon(${x1 - offset}% 0, ${x2 + offset}% 0, ${x1 + offset}% 100%, ${x3 - offset}% 100%)`;
 }
+
+const fullscreen = () => "polygon(0 0, 100% 0, 100% 100%, 0% 100%)";
 
 Slice.propTypes = {
     item: PropTypes.number.isRequired,
+    image: PropTypes.object.isRequired,
     width: PropTypes.number.isRequired,
     isHover: PropTypes.bool.isRequired,
+    isSelected: PropTypes.bool.isRequired,
     onHover: PropTypes.func.isRequired,
     onClick: PropTypes.func.isRequired
 }
