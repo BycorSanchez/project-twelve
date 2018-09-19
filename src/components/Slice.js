252b64e72ba50a7079f207b81b8f18cdcd6c0524
Bycor Sánchez <bycor1989@gmail.com>
2018-09-19 09:41:03 +0100

    refactor: update slice component class style

--- src/components/Slice.js
+++ src/components/Slice.js
@@ -1,48 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
-const Slice = props => {
-    const { image, item, isHover, isSelected, onHover, onClick } = props;
-    const polygon = clipPath(props);
+class Slice extends Component {
 
-    return (
-        <div
-            className={classnames("slice", { "slice-front": (isHover || isSelected) })}
-            style={{
-                clipPath: polygon,
-                WebkitClipPath: polygon
-            }}
-            onMouseEnter={() => onHover(item)}
-            onClick={() => onClick(item)}
-        >
-            <img src={image.url} alt={image.alt}/>
-        </div>
-    );
-}
+    static propTypes = {
+        item: PropTypes.number.isRequired,
+        image: PropTypes.object.isRequired,
+        width: PropTypes.number.isRequired,
+        isHover: PropTypes.bool.isRequired,
+        isSelected: PropTypes.bool.isRequired,
+        onHover: PropTypes.func.isRequired,
+        onClick: PropTypes.func.isRequired
+    }
 
-const clipPath = (props) => {
-    const { item, width, isSelected, isHover } = props;
-    return isSelected ? fullscreen() : polygon(item, width, isHover ? (width / 4) : 0);
-}
+    static fullPolygon = "polygon(0 0, 100% 0, 100% 100%, 0% 100%)";
 
-const polygon = (i, width, offset = 0) => {
-    const x1 = i * width;
-    const x2 = (i + 1) * width;
-    const x3 = (i - 1) * width;
-    return `polygon(${x1 - offset}% 0, ${x2 + offset}% 0, ${x1 + offset}% 100%, ${x3 - offset}% 100%)`;
-}
+    polygon = (i, width, offset = 0) => {
+        const x1 = i * width;
+        const x2 = (i + 1) * width;
+        const x3 = (i - 1) * width;
+        return `polygon(${x1 - offset}% 0, ${x2 + offset}% 0, ${x1 + offset}% 100%, ${x3 - offset}% 100%)`;
+    }
 
-const fullscreen = () => "polygon(0 0, 100% 0, 100% 100%, 0% 100%)";
+    render() {
+        const { image, item, width, isHover, isSelected, onHover, onClick } = this.props;
+        const polygon = isSelected ? Slice.fullPolygon : this.polygon(item, width, isHover ? (width / 4) : 0);;
 
-Slice.propTypes = {
-    item: PropTypes.number.isRequired,
-    image: PropTypes.object.isRequired,
-    width: PropTypes.number.isRequired,
-    isHover: PropTypes.bool.isRequired,
-    isSelected: PropTypes.bool.isRequired,
-    onHover: PropTypes.func.isRequired,
-    onClick: PropTypes.func.isRequired
+        return (
+            <div
+                className={classnames("slice", { "slice-front": (isHover || isSelected) })}
+                style={{
+                    clipPath: polygon,
+                    WebkitClipPath: polygon
+                }}
+                onMouseEnter={() => onHover(item)}
+                onClick={() => onClick(item)}
+            >
+                <img src={image.url} alt={image.alt} />
+            </div>
+        );
+    }
 }
 
 export default Slice;
