04e72889385950f685448e1c5501439ea708189e
Bycor Sánchez <bycor1989@gmail.com>
2018-09-28 14:47:43 +0100

    feat: create clock icon

---
+++ src/components/Clock.js
@@ -0,0 +1,12 @@
+import './Clock.css';
+import React from 'react';
+
+const Clock = () => (
+    <div className="clock">
+        <span className="line hours" />
+        <span className="line minutes" />
+        <span className="clock-center" />
+    </div>
+);
+
+export default Clock;
