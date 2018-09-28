ac9cbb42d736b9f8fdd294166c1be0a9baf4c7d2
Bycor Sánchez <bycor1989@gmail.com>
2018-09-28 10:02:26 +0100

    style: move header tag
    Header tag was located inside front section. Moved to app main tag

--- src/containers/App.js
+++ src/containers/App.js
@@ -33,13 +33,12 @@ class App extends Component {
 
     return (
       <div className="App">
+        <header>
+          <h1>
+            <Rotable text="Memories of" mode="manual" start={hover} options={dataList.map(d => d.title)} />
+          </h1>
+        </header>
         <section className="front">
-          <header>
-            <h1>
-              <Rotable text="Memories of" mode="manual" start={hover} options={dataList.map(d => d.title)} />
-            </h1>
-          </header>
-
           {
             dataList &&
             dataList.map((data, index) => (
