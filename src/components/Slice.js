1558fcb5d1dd4968899876e6bd4100519f84c0ee
Bycor Sánchez <bycor1989@gmail.com>
2018-10-02 09:00:55 +0100

    feat: add slice type property
    Slices can be vertical or horizontal

--- src/components/Slice.js
+++ src/components/Slice.js
@@ -12,15 +12,15 @@ class Slice extends Component {
         isHover: PropTypes.bool,
         isSelected: PropTypes.bool,
         onHover: PropTypes.func,
-        onSelect: PropTypes.func
+        onSelect: PropTypes.func,
+        type: PropTypes.oneOf(['vertical', 'horizontal'])
     }
 
     static defaultProps = {
         isHover: false,
-        isSelected: false
+        isSelected: false,
+        type: 'vertical'
     }
-
-    static fullPolygon = "polygon(0 0, 100% 0, 100% 100%, 0% 100%)";
 
     _callListener(item, func) {
         if (func) {
@@ -29,15 +29,33 @@ class Slice extends Component {
     }
 
     _polygon = (i, width, offset = 0) => {
-        const x1 = i * width;
-        const x2 = (i + 1) * width;
-        const x3 = (i - 1) * width;
-        return `polygon(${x1 - offset}% 0, ${x2 + offset}% 0, ${x1 + offset}% 100%, ${x3 - offset}% 100%)`;
+        const p1 = (i * width) - offset;
+        const p3 = (i * width) + offset;
+        const p2 = ((i + 1) * width) + offset;
+        const p4 = ((i - 1) * width) - offset;
+        return this._polygonOf(p1, p2, p3, p4);
+    }
+
+    _fullPolygon() {
+        return this._polygonOf(0, 100, 100, 0);
+    }
+
+    _polygonOf(...points) {
+        if (points.length < 4) return undefined;
+        return (this.props.type === 'vertical') ? this._verticalPolygon(points) : this._horizontalPolygon(points);
+    }
+
+    _verticalPolygon(points) {
+        return `polygon(${points[0]}% 0, ${points[1]}% 0, ${points[2]}% 100%, ${points[3]}% 100%)`;
+    }
+
+    _horizontalPolygon(points) {
+        return `polygon(0 ${points[0]}%, 0 ${points[1]}%, 100% ${points[2]}%, 100% ${points[3]}%)`;
     }
 
     render() {
         const { item, width, image, isSelected, isHover, onHover, onSelect } = this.props;
-        const polygon = isSelected ? Slice.fullPolygon : this._polygon(item, width, isHover ? (width / 4) : 0);
+        const polygon = isSelected ? this._fullPolygon() : this._polygon(item, width, isHover ? (width / 4) : 0);
 
         return (
             <div className={classnames("slice", { "slice-front": (isSelected || isHover) })}
