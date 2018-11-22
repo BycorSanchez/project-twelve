7a8796372f21bb80ac41fa1f4e8c43076689515a
Bycor Sánchez <bycor1989@gmail.com>
2018-11-22 17:11:46 +0000

    -

--- src/api.js
+++ src/api.js
@@ -1,21 +1,25 @@
 // Get front page data (slice image, title, etc)
-export const fetchFrontData = () => fetch("data/front-data.json").then(r => r.json());
+export const fetchFrontData = () =>
+  fetch("data/front-data.json").then(r => r.json());
 
 export const fetchImages = (query, page = 1) => {
-    return fetch(
-        `https://api.pexels.com/v1/search/?page=${page}&per_page=15&query=${query.replace(/ /g, '+')}`,
-        {
-            method: 'GET',
-            headers: { "Authorization": "XXXX" }
-        })
-        .then(r => r.json())
-        .then(r => r.photos);
-}
+  return fetch(
+    `https://api.pexels.com/v1/search/?page=${page}&per_page=15&query=${query.replace(/ /g,"+")}`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: "XXXX"
+      }
+    }
+  )
+    .then(r => r.json())
+    .then(r => r.photos);
+};
 
 export const imageSize = width => {
-    if (width < 190) return "small";
-    if (width < 500) return "medium";
-    if (width < 940) return "large";
-    if (width > 1500) return "original";
-    else return "large2x";
-}
+  if (width < 190) return "small";
+  if (width < 500) return "medium";
+  if (width < 940) return "large";
+  if (width > 1500) return "original";
+  else return "large2x";
+};
