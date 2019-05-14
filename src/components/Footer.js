e3bee3c99e4e97df307e0fe18a2ba91feec0f995
Bycor Sánchez <bycor1989@gmail.com>
2019-05-14 08:09:40 +0100

    add attributes
    Add links to original image and icon creators/owners

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,14 @@
 import styles from "../styles/Footer.module.css";
 import React from "react";
+import pexels from "../images/pexels.svg";
+import icons from "../images/iconmonstr.svg";
 
 const Footer = () => {
   return (
     <footer>
-      <p>
-        🖼 from <a href="https://www.pexels.com/">Pexels</a>. With{" "}
-        <span className={styles.heart}>♥</span> by{" "}
-        <a href="https://twitter.com/bycorsanchez">Bycor</a>
-      </p>
+      <p>Photos from{" "}<a href="https://www.pexels.com/"><img className={styles.icon}  src={pexels} alt="pexels"/></a></p>
+      <p>Icons by <a href="https://iconmonstr.com/"><img className={styles.icon} src={icons} alt="iconmonstr"/></a></p>
+      <p>Made with{" "}<span className={styles.heart}>♥</span> by{" "}<a href="https://twitter.com/bycorsanchez">Bycor</a></p>
     </footer>
   );
 };
